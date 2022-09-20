4d6a1575ced435d8af1ec0b7c53daca5987d1c55
spencerwilson123321 <spencerwilson123321@gmail.com>
2022-09-20 12:08:36 -0700

    encode.py encodes, need to store msg length next.

--- encode.py
+++ encode.py
@@ -6,35 +6,40 @@ img_height = len(img)
 img_width = len(img[0])
 
 msg = input("Enter a message to hide: ")
-msg_in_binary = ''
+binary_msg = ''
 for char in msg:
     binary = bin(ord(char))[2:]
     if len(binary) != 8:
         binary = '0'*(8-len(binary)) + binary # Padding with leading zeros.
     # print(binary)
-    msg_in_binary += binary
+    binary_msg += binary
 # print(msg_in_binary)
 
 # print(int(binary, 2))
 
-def setLSB(x: int) -> None:
+def setLSB(binary_str: str, x: int) -> None:
     pass
 
-def encode(message, image):
+class Done(Exception): pass
+
+def encode(bin_msg, image):
     bin_msg_index = 0
-    for y in range(0, img_height):
-        for x in range(0, img_width):
-            red = image[y, x][0] # R
-            green = image[y, x][1] # G
-            blue = image[y, x][2] # B
-            rb = bin(red)[2:]
-            bit = message[bin_msg_index]
-            rb = rb[0:7] + bit
-            print(rb)
-            print(int(rb, 2))
-            break
-        break
+    bin_msg_length = len(bin_msg)
+    try:
+        for y in range(0, img_height):
+            for x in range(0, img_width):
+                for colour in image[y, x]:
+                    if bin_msg_index >= bin_msg_length:
+                        raise Done
+                    colour_binary = bin(colour)[2:]
+                    bit = bin_msg[bin_msg_index]
+                    if len(colour_binary) != 8:
+                        colour_binary = '0'*(8-len(colour_binary)) + colour_binary # Padding with leading zeros.
+                    colour_binary = colour_binary[0:7] + bit
+                    print(colour_binary)
+                    bin_msg_index += 1
+    except Done:
+        pass
 
-encode(msg_in_binary, img)
-
-# cv2.imwrite("white_changed.bmp", img)
+encode(binary_msg, img)
+cv2.imwrite("stego_image.bmp", img)
