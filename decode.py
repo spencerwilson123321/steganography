4e38432b9794a894335c17344c0eae8dda5079aa
spencerwilson123321 <spencerwilson123321@gmail.com>
2022-09-20 18:06:55 -0700

    Basic version of encoder and decoder works

--- decode.py
+++ decode.py
@@ -1,16 +1,169 @@
-import cv2
+from sys import stderr
+from cv2 import imread, imwrite
 import numpy as np
 import argparse
+from os.path import isfile
 
 parser = argparse.ArgumentParser("./decode.py")
-parser.add_argument("-f", dest="file", required=True)
+parser.add_argument("INPUT_FILE_PATH")
 args = parser.parse_args()
-# args.x
-# args.y
 
-# 1. Read the file to decode from cmd args.
+if not isfile(args.INPUT_FILE_PATH):
+    print(f"ERROR: File '{args.INPUT_FILE_PATH}' not found.", file=stderr)
+    exit(1)
 
-# img = cv2.imread("white.bmp")
-# img_height = len(img)
-# img_width = len(img[0])
 
+class Done(Exception): pass
+
+
+class Bits():
+
+    def __init__(self, value: str or int, numBits: int):
+        self.__bits = ""
+        if type(value) is str:
+            temp = bin(ord(value))[2:]
+            if len(temp) != numBits:
+                temp = "0"*(numBits-len(temp)) + temp
+            self.__bits = temp
+        elif type(value) is int:
+            temp = bin(value)[2:]
+            if len(temp) != numBits:
+                temp = "0"*(numBits-len(temp)) + temp
+            self.__bits = temp
+        elif type(value) is np.uint8:
+            temp = bin(value)[2:]
+            if len(temp) != numBits:
+                temp = "0"*(numBits-len(temp)) + temp
+            self.__bits = temp
+        self.__len = numBits
+    
+    def __getitem__(self, key):
+        return self.__bits[key]
+    
+    def __repr__(self):
+        return self.__bits
+
+    def setLSB(bits: str) -> None:
+        pass
+
+    def getLSB() -> str:
+        pass
+
+    def getBits(start, end):
+        pass
+
+    def getBit(index):
+        pass
+
+    def toInt() -> int:
+        pass
+
+    def toString() -> str:
+        pass
+
+
+class EncodedImage:
+
+    def __init__(self):
+        self.__imgMatrix = None
+        self.__numColourChannels = 0
+        self.__numPixels = 0
+        self.__width = 0
+        self.__height = 0
+    
+    def loadImage(self, file_path):
+        self.__imgMatrix = imread(file_path)
+        self.__numColourChannels = self.__imgMatrix.shape[2]
+        self.__width = self.__imgMatrix.shape[1]
+        self.__height = self.__imgMatrix.shape[0]
+        self.__numPixels = self.__width * self.__height
+
+    def saveImage(self, file_path):
+        imwrite(file_path, self.__imgMatrix)
+    
+    def getPixel(self, y, x):
+        return self.__imgMatrix[y, x]
+    
+    def setPixel(self, y, x, pixel):
+        self.__imgMatrix[y, x] = pixel
+    
+    def getPixelRow(self, y):
+        return self.__imgMatrix[y]
+
+    def getHeight(self):
+        return self.__height
+    
+    def getWidth(self):
+        return self.__width
+
+    def getNumColourChannels(self):
+        return self.__numColourChannels
+
+    def getNumPixels(self):
+        return self.__numPixels
+
+    def getColourChannel(self, pixel, colour: str):
+        if colour == "red":
+            return pixel[0]
+        elif colour == "green":
+            return pixel[1]
+        elif colour == "blue":
+            return pixel[2]
+
+
+class Decoder:
+
+    def __init__(self):
+        pass
+
+    def decodeMessageSize(self, img: EncodedImage):
+        msg_size = 0
+        # Do something
+        msg_size_decoded = ""
+        try:
+            for x in range(0, img.getWidth()):
+                pixel = img.getPixel(0, x)
+                for colour in ["red", "green", "blue"]:
+                    if len(msg_size_decoded) == 32:
+                        raise Done
+                    colour_intensity = img.getColourChannel(pixel, colour)
+                    bits = Bits(colour_intensity, 8)
+                    encoded_bit = bits[7]
+                    msg_size_decoded += encoded_bit
+        except Done:
+            pass
+        msg_size = int(msg_size_decoded, 2)
+        return msg_size
+
+    def decodeMessage(self, img: EncodedImage, msg_size: int):
+        # Do something
+        message_bits = ""
+        decoded_message = ""
+        try:
+            for y in range(1, img.getHeight()):
+                for x in range(0, img.getWidth()):
+                    pixel = img.getPixel(y, x)
+                    for colour in ["red", "green", "blue"]:
+                        if len(message_bits) == msg_size*8:
+                            raise Done
+                        colour_intensity = img.getColourChannel(pixel, colour)
+                        bits = Bits(colour_intensity, 8)
+                        encoded_bit = bits[7]
+                        message_bits += encoded_bit
+        except Done:
+            pass
+        print(f"Decoded Message in Binary: {message_bits}")
+        for x in range(0, msg_size*8, 8):
+            bits = message_bits[x:x+8]
+            decoded_message += chr(int(bits, 2))
+        return decoded_message
+
+
+if __name__ == "__main__":
+    image = EncodedImage()
+    decoder = Decoder()
+    image.loadImage(args.INPUT_FILE_PATH)
+    msg_size = decoder.decodeMessageSize(image)
+    message = decoder.decodeMessage(image, msg_size)
+    print(f'Decoded Message Size: {msg_size} Bytes')
+    print(f'Decoded Message: "{message}"')
--- encode.py
+++ encode.py
@@ -1,45 +1,91 @@
+from lib2to3.pytree import convert
 import cv2
 import numpy as np
+import argparse
+from sys import exit, stderr
+from os.path import isfile
 
-img = cv2.imread("white.bmp")
-img_height = len(img)
-img_width = len(img[0])
+# Command Line Arguments
+parser = argparse.ArgumentParser("./encode.py")
 
-msg = input("Enter a message to hide: ")
-binary_msg = ''
-for char in msg:
-    binary = bin(ord(char))[2:]
-    if len(binary) != 8:
-        binary = '0'*(8-len(binary)) + binary # Padding with leading zeros.
-    # print(binary)
-    binary_msg += binary
-# print(msg_in_binary)
+parser.add_argument("INPUT_FILE_PATH")
+parser.add_argument("-o", dest="OUTPUT_FILE_PATH", required=False, default="stego_image.bmp")
+args = parser.parse_args()
 
-# print(int(binary, 2))
+# Verify inputs
+if not isfile(args.INPUT_FILE_PATH):
+    print(f"ERROR: Input file '{args.INPUT_FILE_PATH}' not found", file=stderr)
+    exit(1)
 
-def setLSB(binary_str: str, x: int) -> None:
-    pass
+def int_to_binary(num: int, num_bits: int):
+    binary = bin(num)[2:]
+    if len(binary) != num_bits:
+        binary = "0"*(num_bits-len(binary)) + binary
+    return binary
+
+def binary_to_int(binary_number: str):
+    return int(binary_number, 2)
 
 class Done(Exception): pass
 
 def encode(bin_msg, image):
     bin_msg_index = 0
-    bin_msg_length = len(bin_msg)
+    bin_msg_len = len(bin_msg)
+    height = len(image)
+    width = len(image[0])
+
+    # Encode message size into the first 32 LSBs of the picture.
+    # Message size will be stored as a 32 bit integer
+    # Unit for message size is BYTES.
+    # Therefore, the 32 bit message size integer can represent a message size of up to 4,294,967,296 bytes or 4096 MB
+    # which is more than enough space.
+    
+    # Converting the message length from bytes to binary representation (str).
+    msg_len_as_binary = int_to_binary(int(len(bin_msg) / 8), 32) # divide by 8 for bytes.
+    # Encode the message length as binary into the first 32 LSBs of the image.
     try:
-        for y in range(0, img_height):
-            for x in range(0, img_width):
-                for colour in image[y, x]:
-                    if bin_msg_index >= bin_msg_length:
-                        raise Done
-                    colour_binary = bin(colour)[2:]
-                    bit = bin_msg[bin_msg_index]
-                    if len(colour_binary) != 8:
-                        colour_binary = '0'*(8-len(colour_binary)) + colour_binary # Padding with leading zeros.
-                    colour_binary = colour_binary[0:7] + bit
-                    print(colour_binary)
-                    bin_msg_index += 1
+        y = 0
+        index = 0
+        for x in range(0, width): # The entire first row of pixels in the image will be reserved for metadata for now.
+            colour_index = 0
+            for colour in image[y, x]:
+                if index >= 32:
+                    raise Done
+                colour_binary = int_to_binary(colour, 8)
+                bit = msg_len_as_binary[index]
+                colour_binary = colour_binary[0:7] + bit
+                image[y, x][colour_index] = binary_to_int(colour_binary)
+                index += 1
+                colour_index += 1
     except Done:
         pass
 
-encode(binary_msg, img)
-cv2.imwrite("stego_image.bmp", img)
+    try:
+        for y in range(1, height): # Start at second row to avoid metadata.
+            for x in range(0, width):
+                colour_index = 0
+                for colour in image[y, x]:
+                    if bin_msg_index >= bin_msg_len:
+                        raise Done
+                    colour_binary = int_to_binary(colour, 8)                    # RGB value converted to binary notation (str)
+                    bit = bin_msg[bin_msg_index]                                # Get next bit of message using msg index.
+                    colour_binary = colour_binary[0:7] + bit                    # Replace the LSB of the RGB value to our message bit.
+                    image[y, x][colour_index] = binary_to_int(colour_binary)    # Replace the old RGB value with the encoded RGB value.
+                    bin_msg_index += 1
+                    colour_index += 1
+    except Done:
+        pass
+
+if __name__ == "__main__":
+
+    img = cv2.imread(args.INPUT_FILE_PATH)
+    msg = input("Enter a message to hide: ")
+    binary_msg = ''
+    for char in msg:
+        binary = bin(ord(char))[2:]
+        if len(binary) != 8:
+            binary = '0'*(8-len(binary)) + binary # Padding with leading zeros.
+        binary_msg += binary
+
+    encode(binary_msg, img)
+    cv2.imwrite(args.OUTPUT_FILE_PATH, img)
